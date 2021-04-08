d113e18b3fb057a09b96ae6d8220d79dc681a0fc
Desmond <b22191774@gmail.com>
2021-04-08 17:20:21 +0800

    0408_2

--- jsx/postList.jsx
+++ jsx/postList.jsx
@@ -1,30 +1,46 @@
 import React, {Component} from 'react';
 import PostData from '../JSON/1/test.json';
+let ar = 1;
+let pp = PostData[ar].ex;
 
 class PostList extends Component {
-    
+
+  constructor(props) {
+    super(props);
+
+  }
+
   render() {
-    return (
-      <div>
-        <h1>YES!!</h1>
-        <h1>{PostData[3].ab}</h1>
-        
-        {PostData.map((postDetail, index) => {
-          return (
-           
-            <div key={postDetail.n}>
-            
-              <h2 value={postDetail.en}>{postDetail.ab[0]}</h2>
-                
-            </div>
-          )
-        })}
-       
-        
-      </div>
-      
-    )
-    
+    if (pp == false) {
+      console.log('yes')
+      return (
+
+        <div>
+          <h1>YES!!</h1>
+          <h1>{PostData[3].ab}</h1>
+
+          {PostData.map((postDetail, index) => {
+            return (
+
+              <div key={postDetail.n}>
+
+                <h2 value={postDetail.en}>{postDetail.ab[0]}</h2>
+
+              </div>
+            )
+          })}
+
+        </div>
+
+      )
+    } else {
+      console.log(pp)
+      return (
+        <p>no</p>
+      )
+    }
+
+
   }
 
 }
